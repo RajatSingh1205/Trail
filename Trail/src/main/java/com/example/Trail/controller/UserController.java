d12770a441995ce3bde69ed8329ca8d512ae5a51
ichirokun12 <rajat.k.singh1209@gmail.com>
2026-01-08 21:17:01 +0530

    user api

--- Trail/src/main/java/com/example/Trail/controller/UserController.java
+++ Trail/src/main/java/com/example/Trail/controller/UserController.java
@@ -1,4 +1,37 @@
 package com.example.Trail.controller;
 
+import com.example.Trail.entity.User;
+import com.example.Trail.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserServices userServices;
+
+    @PostMapping("/addUser")
+    public ResponseEntity<?> userRegistration(@RequestBody User user) {
+        User savedUser = userServices.addUser(user);
+        return new ResponseEntity<>(savedUser,HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getAllUsers")
+    public ResponseEntity<?> getAllUser() {
+        List<User> users = userServices.getAllUser();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/getUserById/{userId}")
+    public ResponseEntity<?> getUserById(@PathVariable Long userId) {
+        Optional<User> user = userServices.getUserById(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 }
--- Trail/src/main/java/com/example/Trail/repository/UserRepository.java
+++ Trail/src/main/java/com/example/Trail/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.Trail.repository;
 
-public interface UserRepository {
+import com.example.Trail.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
